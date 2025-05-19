88b6a5014382e919258b661af5f4c2b3391bd128
Cayke Daniel <contato.cayke@gmail.com>
2025-05-19 17:46:11 -0300

    feat: remove ununsed envs and add readme

--- src/config/env.ts
+++ src/config/env.ts
@@ -4,7 +4,6 @@ dotenv.config();
 export const env = {
   discordToken: process.env.DISCORD_BOT_TOKEN || "",
   port: Number(process.env.PORT) || 3005,
-  serenoChannel: process.env.SERENO_CHANNEL_ID || "",
   upstashRedisUrl: process.env.UPSTASH_REDIS_REST_URL || "",
   upstashRedisToken: process.env.UPSTASH_REDIS_REST_TOKEN || "",
 };
--- src/projectMap.ts
+++
@@ -1,5 +0,0 @@
-import { env } from "./config/env";
-
-export const projectChannelMap: Record<string, string> = {
-  sereno: env.serenoChannel,
-};
